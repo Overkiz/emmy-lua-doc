e34c091537d3f3d1b6e2c482c8a4c1910d4f121a
boolangery <eliott.dumeix@gmail.com>
2019-01-28 16:31:35 +0100

    update timer doc.

--- Overkiz/Timer.lua
+++ Overkiz/Timer.lua
@@ -1,11 +1,25 @@
 local Timer = {}
 
---- @class Overkiz.Timer.Monotonic
+---@class Overkiz.Timer.Monotonic
+---@field public expired fun():void
 local Monotonic = {}
 
 ---@return Overkiz.Timer.Monotonic
 function Timer.Monotonic()
   return Monotonic()
+end
+
+--- Start the timer.
+function Monotonic:start() end
+
+--- Stop the timer.
+function Monotonic:stop() end
+
+---@param time Overkiz.Time.Elapsed
+---@param absolute boolean
+---@overload fun(time: Overkiz.Time.Elapsed)
+function Monotonic:setTime(time, absolute)
+
 end
 
 return Timer
