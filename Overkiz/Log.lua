43dfd3de009af26b37f048bff763cbcc7469ca3e
boolangery <eliott.dumeix@gmail.com>
2019-01-29 09:42:00 +0100

    add Log.

---
+++ Overkiz/Log.lua
@@ -0,0 +1,22 @@
+--- @class Overkiz.Log
+local Log = {}
+
+--- @param msg string log message
+function Log:debug(msg) end
+
+--- @param msg string log message
+function Log:error(msg) end
+
+--- @param msg string log message
+function Log:warning(msg) end
+---
+--- @param msg string log message
+function Log:info(msg) end
+
+--- @param msg string log message
+function Log:notice(msg) end
+
+--- @param msg string log message
+function Log:fatal(msg) end
+
+return Log
