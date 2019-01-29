cd73ca450d1978c8ef8c434acba259b1d1ec3795
boolangery <eliott.dumeix@gmail.com>
2019-01-29 10:28:36 +0100

    add Subject and TableStorage.

---
+++ Overkiz/Subject.lua
@@ -0,0 +1,18 @@
+--- @class Overkiz.Subject
+local Subject = {}
+
+--- @class Overkiz.Listener
+--- @field public notified fun(...)
+local Listener = {}
+
+--- @vararg any args to be send to listener
+function Subject:notify(...) end
+
+--- @param listener fun(...)|Overkiz.Listener
+--- @return number listener index (used to remove listener)
+function Subject:addListener(listener) end
+
+--- @param listener number|Overkiz.Listener
+function Subject:removeListener(listener) end
+
+return Subject
